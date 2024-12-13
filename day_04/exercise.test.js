43bfb6c1effff82c18765ec7b5096e3f3acfe304
Tomas Orti <134451827+tomiiorti@users.noreply.github.com>
2024-12-13 11:25:03 -0300

    Exercise 4, update

--- day_04/exercise.test.js
+++ day_04/exercise.test.js
@@ -18,50 +18,9 @@ describe('Decorando el árbol de Navidad', () => {
         return result.trim();
     };
 
-    /**
-     * function createXmasTree(height, ornament) {
-        let result = '';
-        if (height < 1 || height > 100) {
-            throw Error("La altura del árbol debe ser un entero entre el 1 y 100");
-        };
-        const totalWidth = 1 + (height - 1) * 2;
-        for(let i = 0; i < height; i++ ) {
-            const ornamentPerRow = ornament.repeat(1 + i * 2);
-            const spacesNeeded = (totalWidth - ornamentPerRow.length) / 2;
-            const rows = `${ornamentPerRow}`
-            result += rows + '\n';
-        }
-        return result.trim();
-    };
-     */
-
     it('should be error if the height is more than 100', () => {
         expect(() => createXmasTree(101, '*')).toThrow("La altura del árbol debe ser un entero entre el 1 y 100")
     });
-/**
- *     it('should print the ornament in scale', () => {
-        const expected = 
-`*
-***
-*****
-*******
-*********`;
-        const tree = createXmasTree(5, '*');
-        expect(tree).toEqual(expected);
-    });
- */
-/**
- *     it('should print the ornament in scale', () => {
-        const expected = 
-`____*____
-___***___
-__*****__
-_*******_
-*********`;
-        const tree = createXmasTree(5, '*');
-        expect(tree).toEqual(expected);
-    });
- */
     it('should print the ornament in scale', () => {
         const expected = 
 `____*____
@@ -99,4 +58,3 @@ _____#_____`;
     });
 
 })
-
