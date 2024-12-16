74a9e069ee96f20561527e95aadc6925fa85ef09
Tomas Orti <134451827+tomiiorti@users.noreply.github.com>
2024-12-16 15:41:08 -0300

    Exercise 8, done

---
+++ day_08/exercise.test.js
@@ -0,0 +1,75 @@
+describe('La carrera de los renos', () => {
+    function drawRace(indices, length) {
+        let result = '';
+        function createTrack (length) {
+            const string = `${'~'.repeat(length)}` 
+            return string;
+        };
+        const track = createTrack(length);
+
+        for (let i = 0; i < indices.length; i++) {
+            let modString = '';
+
+            if (indices[i] > 0) {
+                modString = track.slice(0, indices[i]) + 'r' + track.slice(indices[i] + 1);
+            } else if (indices[i] < 0) {
+                const posFromEnd = length + indices[i];
+                modString = track.slice(0, posFromEnd) + 'r' + track.slice(posFromEnd + 1);            
+            } else {
+                modString = track;
+            };
+
+            result += ' '.repeat(indices.length - i - 1) + modString + ` /${i + 1}`;
+            
+            if (i < indices.length - 1) {
+                result += '\n';
+            };            
+        };
+        
+        return result;
+    };
+
+    it('should return the length of the track', () => {
+        const race = drawRace([0], 10);
+        const expected = '~~~~~~~~~~ /1';
+
+        expect(race).toEqual(expected);
+    });
+
+    it('debería colocar el reno en una posición positiva', () => {
+        const result = drawRace([5], 10);
+        const expected = '~~~~~r~~~~ /1';
+        expect(result).toEqual(expected);
+    });
+
+    it('debería posicionar correctamente los renos con índices positivos y negativos', () => {
+        const race = drawRace([0, 5, -3], 10);
+        const expected =
+            '  ~~~~~~~~~~ /1\n' +
+            ' ~~~~~r~~~~ /2\n' +
+            '~~~~~~~r~~ /3';
+
+        expect(race).toEqual(expected);
+    });
+
+    it('debería funcionar con múltiples índices mixtos', () => {
+        const race = drawRace([2, -1, 0, 5], 8);
+        const expected =
+            '   ~~r~~~~~ /1\n' +
+            '  ~~~~~~~r /2\n' +
+            ' ~~~~~~~~ /3\n' +
+            '~~~~~r~~ /4';
+
+        expect(race).toEqual(expected);
+    });
+
+    it('debería manejar una pista más larga con diferentes índices', () => {
+        const race = drawRace([3, 7, -2], 12);
+        const expected =
+            '  ~~~r~~~~~~~~ /1\n' +
+            ' ~~~~~~~r~~~~ /2\n' +
+            '~~~~~~~~~~r~ /3';
+
+        expect(race).toEqual(expected);
+    });
+});
